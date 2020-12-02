2a2b605cb12ec57d5786eb984214dc9c6111dd8f
Caspento <Mrcaspento@gmail.com>
2020-12-02 18:42:48 -0500

    inquirer prompts

--- app.js
+++ app.js
@@ -13,7 +13,26 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+class App {
+    constructor(){
+        this.employees = [];
+        this.employeePrompt = [
+            {
+                type: "input",
+                message: "What is your name?",
+                name: "name"
+            },{
+                type: "input",
+                message: "What is your email?",
+                name: "email"
+            },{
+                type: "input",
+                message: "Do you have an ID and what is it?",
+                name: "id"
+            }
+        ];
+    }
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!

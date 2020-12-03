39baea09472e7e20b467c1d22b92c98f3b4f50de
Caspento <Mrcaspento@gmail.com>
2020-12-02 22:05:52 -0500

    redo

--- app.js
+++ app.js
@@ -1,152 +1,153 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+const Employee = require("./lib/Employee")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const { OutgoingMessage } = require("http");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+let teamArray = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function buildPage() {
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
-}
+addManager();
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your team manager's name?",
+            name: "name"
+        }, {
+            type: "input",
+            message: "What is your id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your team manager's email address?",
+            name: "email"
+        },
 
-class App {
-    constructor() {
-        this.employees = [];
-        this.employeePrompt = [
-            {
-                type: "input",
-                message: "What is your name?",
-                name: "name"
-            }, {
-                type: "input",
-                message: "What is your email?",
-                name: "email"
-            }, {
-                type: "input",
-                message: "Do you have an ID and what is it?",
-                name: "id"
-            }
-        ];
-
-        this.engineerPrompt = this.employeePrompt.concat([
-            {
-                type: "input",
-                message: "Whar is your github?",
-                name: "github"
-            }
-        ]);
-
-        this.internPrompt = this.employeePrompt.concat([
-            {
-                type: "input",
-                message: "What school do you go to?",
-                name: "school"
-            }
-        ]);
-
-        this.managerPrompt = this.employeePrompt.concat([
-            {
-                type: "input",
-                message: "what is your office number?",
-                name: "officeNumber"
-            }
-        ]);
-    }
-    //start
-    createTeam() {
-        this.newEmployee(); //next.Employee
-    }
-    //end 
-    finishTeam() {
-        console.log("You're overbudget! Fire somone!")
-    }
-
-//next Employee
-    newEmployee() {
-
-        this.teamCreation().then((teamChoice) => {
-            if (teamChoice === "finishTeam") {
-                this.render();
-                this.finishTeam();
-            } else {
-                this.teamCreation();
-                this.promptData(teamChoice).then((data) => {
-                    switch (teamChoice) {
-                        case "Engineer":
-                            this.employees.push(new Engineer(data.name, data.id, data.email, data.github));
-                            break;
-                        case "Intern":
-                            this.employees.push(new Intern(data.name, data.id, data.email, data.school));
-                            break;
-                        case "Manager":
-                            this.employees.push(new Manager(data.name, data.id, data.email, data.officeNumber));
-                            break;
-                        default:
-                            buildPage();
-                    }
-
-                })
-            }
-        });
-
-    }
-
-    teamCreation = () => {
-
-        return inquirer.prompt([
-            {
-                type: "list",
-                message: "Who would you like to add to the team?",
-                name: "teamChoice",
-                choices: [
-                    "Engineer",
-                    "Intern",
-                    "Manager",
-                    "finishTeam"
-                ]
-            }
-        ]).then(function (data) {
-            return (data.teamChoice);
-        }).catch(function (error) {
-            console.log(error);
-        });
-    };
-
-    promptInfo(teamChoice) {
-        switch (teamChoice) {
-            case "Engineer":
-                return inquirer.prompt(this.engineerPrompt).then(function (data) {
-                    return data;
-                });
-            case "Intern":
-                return inquirer.prompt(this.internPrompt).then(function (data) {
-                    return data;
-                });
-            case "Manager":
-                return inquirer.prompt(this.mangerPrompt).then(function (data) {
-                    return data;
-                });
+        {
+            type: "number",
+            message: "What is your team manager's office number?",
+            name: "officeNumber"
         }
-    }
-
-    render(){
-        fs.readFile('templates/main.html', 'utf8',(err,htmlString) => {
-            htmlString = htmlString.split("<script></script>").join(this.getScript)
-        })
-    }
-
-
-
+    ]).then(function (data) {
+        const name = data.name;
+        const id = 420;
+        const email = data.email;
+        const officeNumber = data.officeNumber;
+        const teamMember = new Manager(name, id, email, officeNumber);
+        teamArray.push(teamMember);
+        addTeamMembers();
+    });
 }
+
+function addTeamMembers(data) {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Who would you like to work to death?",
+            choices: ["A hopless Intern", "A suspicous Engineer", "None because you're broke"],
+            name: "addCoworker"//addmemberdata
+        }
+    ]).then(function (data) {
+        switch (data.addCoWorker) {
+            case "A hopless Intern":
+                addIntern();
+                break;
+            case "A suspicous Engineer":
+                addEngineer();
+                break;
+            case "None because you're broke":
+                completeTeam();
+                break;
+        }
+
+    })
+}
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this noobs name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is this intern's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is this intern's school?",
+            name: "school"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = finalTeamArray.length + 1
+            const email = data.email
+            const school = data.school
+            const teamMember = new Intern(name, id, email, school)
+            teamArray.push(teamMember)
+            addTeamMembers()
+        });
+};
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is this engineer's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is this engineer's Github profile?",
+            name: "github"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = finalTeamArray.length + 1
+            const email = data.email
+            const github = data.github
+            const teamMember = new Engineer(name, id, email, github)
+            teamArray.push(teamMember)
+            addTeamMembers()
+        });
+};
+
+function teamDone(){
+    if(!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR)
+    }fs.writeFileSync(outputPath, render(teamArray), "utf-8");
+}
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -163,4 +164,4 @@ class App {
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work! ``

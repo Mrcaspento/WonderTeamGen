e918b9dc9227cf4a27b93cca5d793cf480b0bff0
Caspento <Mrcaspento@gmail.com>
2020-12-02 20:15:42 -0500

    adding render

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -13,19 +14,23 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function buildPage() {
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
+}
+
 class App {
-    constructor(){
+    constructor() {
         this.employees = [];
         this.employeePrompt = [
             {
                 type: "input",
                 message: "What is your name?",
                 name: "name"
-            },{
+            }, {
                 type: "input",
                 message: "What is your email?",
                 name: "email"
-            },{
+            }, {
                 type: "input",
                 message: "Do you have an ID and what is it?",
                 name: "id"
@@ -50,27 +55,105 @@ class App {
 
         this.managerPrompt = this.employeePrompt.concat([
             {
-                type:"input",
+                type: "input",
                 message: "what is your office number?",
                 name: "officeNumber"
             }
         ]);
+    }
+    //start
+    createTeam() {
+        this.newEmployee(); //next.Employee
+    }
+    //end 
+    finishTeam() {
+        console.log("You're overbudget! Fire somone!")
+    }
 
+//next Employee
+    newEmployee() {
 
+        this.teamCreation().then((teamChoice) => {
+            if (teamChoice === "finishTeam") {
+                this.render();
+                this.finishTeam();
+            } else {
+                this.teamCreation();
+                this.promptData(teamChoice).then((data) => {
+                    switch (teamChoice) {
+                        case "Engineer":
+                            this.employees.push(new Engineer(data.name, data.id, data.email, data.github));
+                            break;
+                        case "Intern":
+                            this.employees.push(new Intern(data.name, data.id, data.email, data.school));
+                            break;
+                        case "Manager":
+                            this.employees.push(new Manager(data.name, data.id, data.email, data.officeNumber));
+                            break;
+                        default:
+                            buildPage();
+                    }
 
-
+                })
+            }
+        });
 
     }
+
+    teamCreation = () => {
+
+        return inquirer.prompt([
+            {
+                type: "list",
+                message: "Who would you like to add to the team?",
+                name: "teamChoice",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Manager",
+                    "finishTeam"
+                ]
+            }
+        ]).then(function (data) {
+            return (data.teamChoice);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    promptInfo(teamChoice) {
+        switch (teamChoice) {
+            case "Engineer":
+                return inquirer.prompt(this.engineerPrompt).then(function (data) {
+                    return data;
+                });
+            case "Intern":
+                return inquirer.prompt(this.internPrompt).then(function (data) {
+                    return data;
+                });
+            case "Manager":
+                return inquirer.prompt(this.mangerPrompt).then(function (data) {
+                    return data;
+                });
+        }
+    }
+
+    render(){
+        fs.readFile('templates/main.html', 'utf8',(err,htmlString) => {
+            htmlString = htmlString.split("<script></script>").join(this.getScript)
+        })
+    }
+
+
+
 }
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+
+
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
